c306e9122ffefed100b029d5f25af9f0ea7354a6
SouravBanikinmobi <sourav.banik@inmobi.com>
2019-02-25 20:49:29 +0530

    implemented custom param latlong

--- app/src/main/java/com/inmobi/showcase/MainActivity.java
+++ app/src/main/java/com/inmobi/showcase/MainActivity.java
@@ -1,8 +1,10 @@
 package com.inmobi.showcase;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -17,17 +19,32 @@ import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 
 public class MainActivity extends AppCompatActivity {
     Button Load;
+//    public String GDPR;
+//    public String gdpr_consent_available;
+    public String Lat;
+    public String Lon;
+    public String Age;
+
     private static final String TAG = "Interstitial";
     private PublisherInterstitialAd mPublisherInterstitialAd;
+    PublisherAdRequest adRequest ;
+            //.addCustomTargeting("gdpr", "null")
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Load=(Button) findViewById(R.id.load);
+        Lat="0.1";
+        Lon="0.9";   // I passed the latlong as a string . Publisher need to get the value's using some api or by their app code and pass it in these variables.
+        Age="23";    // Publisher need to get the value's from the user and pass it . 
+        adRequest = new PublisherAdRequest.Builder().addCustomTargeting("lat",Lat).addCustomTargeting("lon",Lon).addCustomTargeting("age",Age).build();
+
 
         mPublisherInterstitialAd = new PublisherInterstitialAd(this);
-        mPublisherInterstitialAd.setAdUnitId("/82109981/test_sourav_fullscreen");
+        mPublisherInterstitialAd.setAdUnitId("/82109981/dfp_jsac_int_sourav");
+
 
         mPublisherInterstitialAd.setAdListener(new AdListener() {
             @Override
@@ -62,9 +79,11 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
+
     public  void loadAd(View v){
 
-        mPublisherInterstitialAd.loadAd(new PublisherAdRequest.Builder().build());
+        mPublisherInterstitialAd.loadAd(adRequest);
 
     }
 
